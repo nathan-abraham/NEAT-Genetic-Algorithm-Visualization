76846cc81828439c884192d117b88ae6ec63c9a3
Nathan Abraham <abnathan123@gmail.com>
2021-01-01 22:29:57 -0500

    Added comments and fixed formatting

--- cactus.py
+++ cactus.py
@@ -1,19 +1,22 @@
 import pygame
 import os
 
+# Load images
 CACTI_IMGS = [pygame.image.load(os.path.join("data", "cactusBig0000.png")).convert_alpha(),
-            pygame.image.load(os.path.join("data", "cactusSmall0000.png")).convert_alpha(),
-            pygame.image.load(os.path.join("data", "cactusSmallMany0000.png")).convert_alpha()]
+              pygame.image.load(os.path.join("data", "cactusSmall0000.png")).convert_alpha(),
+              pygame.image.load(os.path.join("data", "cactusSmallMany0000.png")).convert_alpha()]
+
 
 class Cactus:
-    GAP = 200
-    VEL = 14
+    VEL = 14 # Initial velocity
 
     def __init__(self, x, type):
         self.x = x
         self.y = 495
         self.height = 0
         self.passed = False
+
+        # Determines the type of the cactus
         if type == 0:
             self.IMG = CACTI_IMGS[0]
             self.y = 475
@@ -31,16 +34,20 @@ class Cactus:
         win.blit(self.IMG, (self.x, self.y))
 
     def select_draw(self, win, man):
-        pygame.draw.rect(win, (0, 0, 255), (round(self.x), round(self.y), self.IMG.get_width(), self.IMG.get_height()), 3)
-        #win.blit(self.IMG, (self.x, self.y))
+        # Draws the line between the dino and the cactus for visualization purposes
+        pygame.draw.rect(win, (0, 0, 255), (round(self.x), round(self.y),
+                                            self.IMG.get_width(), self.IMG.get_height()), 3)
         pygame.draw.line(win, (0, 0, 255), (man.x + 96, man.y), (self.x, self.y))
 
     def collide(self, man):
+        # Gets the masks from the images
         man_mask = man.get_mask()
         cactus_mask = pygame.mask.from_surface(self.IMG)
 
+        # Calculates an offset necessary for the collision
         offset = (self.x - man.x, self.y - round(man.y))
 
+        # Detects if the masks overlaps
         collision = man_mask.overlap(cactus_mask, offset)
 
         if collision:
