67c2cd3759ac05105a172bc5454251b79e7ce7bc
Nathan Abraham <abnathan123@gmail.com>
2021-01-10 15:58:53 -0500

    Added control activation function

--- activation.py
+++ activation.py
@@ -1,4 +1,4 @@
 from random import random
 
 def control_activation(x):
-    return random() # Returns and random value between 0 and 1
+    return x #random() # Returns and random value between 0 and 1
