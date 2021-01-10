3f51be0b1b6af656e9db99cf67be0d61287dc2c8
Nathan Abraham <abnathan123@gmail.com>
2021-01-10 16:07:34 -0500

    Added no activation

--- activation.py
+++ activation.py
@@ -1,4 +1,7 @@
 from random import random
 
 def control_activation(x):
-    return x #random() # Returns and random value between 0 and 1
+    return random() # Returns and random value between 0 and 1
+
+def no_activation(x):
+    return x
